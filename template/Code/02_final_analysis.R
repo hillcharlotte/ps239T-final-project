92e2c473e7d4d5acaa73875718def6c7afc16e4e
hillcharlotte <35476905+hillcharlotte@users.noreply.github.com>
2018-04-21 12:09:59 -0700

    Update 02_final_analysis.R with more annotations
    Made code clearer with more annotations

--- template/Code/02_final_analysis.R
+++ template/Code/02_final_analysis.R
@@ -44,13 +44,13 @@ seattle_occ_2017$OtherPrivSector <- 0
 
 # Use the information in the OccupationType column to determine whether the voucher should be a 0 or 1.
 for (i in 1:length(seattle_occ_2017$OtherPrivSector)) {
-  if (seattle_occ_2017$OccupationType[i]=="Other Private-Sector") {
-    seattle_occ_2017$OtherPrivSector[i] <- 1
+  if (seattle_occ_2017$OccupationType[i]=="Other Private-Sector") { # If the occupation type is "Other Private-Sector"...
+    seattle_occ_2017$OtherPrivSector[i] <- 1 # ...then replace the 0 in this column with a 1.
   }
 }
 
-# Repeat the above steps for each of the dummy variables:
-
+# Do the same for the other occupation types. First, create the dummy and set it to zero. 
+# Then, use the information in the OccupationType column to determine whether the voucher should be a 0 or a 1.
 seattle_occ_2017$Retired <- 0
 for (i in 1:length(seattle_occ_2017$Retired)) {
   if (seattle_occ_2017$OccupationType[i]=="Retired") {
@@ -58,6 +58,7 @@ for (i in 1:length(seattle_occ_2017$Retired)) {
   }
 }
 
+# Create the dummy and set it to zero. Then use the information in OccupationType to determine whether Voucher should be 0 or 1.
 seattle_occ_2017$Service <- 0
 for (i in 1:length(seattle_occ_2017$Service)) {
   if (seattle_occ_2017$OccupationType[i]=="Service & Nonprofit") {
@@ -65,6 +66,7 @@ for (i in 1:length(seattle_occ_2017$Service)) {
   }
 }
 
+# Create the dummy and set it to zero. Then use the information in OccupationType to determine whether Voucher should be 0 or 1.
 seattle_occ_2017$Lawyers <- 0
 for (i in 1:length(seattle_occ_2017$Lawyers)) {
   if (seattle_occ_2017$OccupationType[i]=="Lawyers") {
@@ -72,6 +74,7 @@ for (i in 1:length(seattle_occ_2017$Lawyers)) {
   }
 }
 
+# Create the dummy and set it to zero. Then use the information in OccupationType to determine whether Voucher should be 0 or 1.
 seattle_occ_2017$Government <- 0
 for (i in 1:length(seattle_occ_2017$Government)) {
   if (seattle_occ_2017$OccupationType[i]=="Government") {
@@ -79,6 +82,7 @@ for (i in 1:length(seattle_occ_2017$Government)) {
   }
 }
 
+# Create the dummy and set it to zero. Then use the information in OccupationType to determine whether Voucher should be 0 or 1.
 seattle_occ_2017$Labor <- 0
 for (i in 1:length(seattle_occ_2017$Labor)) {
   if (seattle_occ_2017$OccupationType[i]=="Blue-Collar & Labor") {
@@ -86,6 +90,7 @@ for (i in 1:length(seattle_occ_2017$Labor)) {
   }
 }
 
+# Create the dummy and set it to zero. Then use the information in OccupationType to determine whether Voucher should be 0 or 1.
 seattle_occ_2017$Technology <- 0
 for (i in 1:length(seattle_occ_2017$Technology)) {
   if (seattle_occ_2017$OccupationType[i]=="Technology") {
@@ -93,6 +98,7 @@ for (i in 1:length(seattle_occ_2017$Technology)) {
   }
 }
 
+# Create the dummy and set it to zero. Then use the information in OccupationType to determine whether Voucher should be 0 or 1.
 seattle_occ_2017$Health <- 0
 for (i in 1:length(seattle_occ_2017$Health)) {
   if (seattle_occ_2017$OccupationType[i]=="Health") {
@@ -100,6 +106,7 @@ for (i in 1:length(seattle_occ_2017$Health)) {
   }
 }
 
+# Create the dummy and set it to zero. Then use the information in OccupationType to determine whether Voucher should be 0 or 1.
 seattle_occ_2017$RealEstate <- 0
 for (i in 1:length(seattle_occ_2017$RealEstate)) {
   if (seattle_occ_2017$OccupationType[i]=="Real Estate") {
@@ -107,6 +114,7 @@ for (i in 1:length(seattle_occ_2017$RealEstate)) {
   }
 }
 
+# Create the dummy and set it to zero. Then use the information in OccupationType to determine whether Voucher should be 0 or 1.
 seattle_occ_2017$Unemployed <- 0
 for (i in 1:length(seattle_occ_2017$Unemployed)) {
   if (seattle_occ_2017$OccupationType[i]=="Unemployed") {
@@ -114,6 +122,7 @@ for (i in 1:length(seattle_occ_2017$Unemployed)) {
   }
 }
 
+# Create the dummy and set it to zero. Then use the information in OccupationType to determine whether Voucher should be 0 or 1.
 seattle_occ_2017$Finance <- 0
 for (i in 1:length(seattle_occ_2017$Finance)) {
   if (seattle_occ_2017$OccupationType[i]=="Finance & Insurance") {
@@ -149,7 +158,7 @@ summary(reg_dummies)
     # First, reorder Occupation Type by total amount given
     seattle_occ_2015$OccupationType <- reorder(seattle_occ_2015$OccupationType, seattle_occ_2015$Amount, sum)
 
-    # Create the plot: 
+    # Using dataset "seattle_occ_2015", set x to OccupationType and y to Amount, fill by Voucher level, and label the levels.
     plot_amount_by_occupation_2015 <- ggplot(seattle_occ_2015, aes(x = OccupationType, y = Amount, fill = factor(Voucher, levels = c(1, 0), labels = c("Voucher", "Non-Voucher")))) +
       geom_bar(stat = "identity") +
       scale_fill_discrete(name = "Contribution Type") + # label the legend
@@ -166,19 +175,20 @@ summary(reg_dummies)
     # Reorder Occupation Type by # of contributions given, using Cycle because it's a standard number for each observation:
     seattle_occ_2015$OccupationType <- reorder(seattle_occ_2015$OccupationType, seattle_occ_2015$Cycle, sum)
 
-    plot_donations_by_occupation_2015 <- ggplot(seattle_occ_2015, aes(OccupationType, fill = factor(Voucher, levels = c(1, 0), labels = c("Voucher", "Non-Voucher")))) + 
+    # Using dataset "seattle_occ_2015", set x to OccupationType, fill by Voucher level, and label the levels.
+    ggplot(seattle_occ_2015, aes(OccupationType, fill = factor(Voucher, levels = c(1, 0), labels = c("Voucher", "Non-Voucher")))) + 
       geom_bar() +
       scale_fill_discrete(name = "Contribution Type") + # label the legend
       labs(title = "2015 Voucher vs. Non-Voucher Contributions by Occupation Type", 
            x = "Occupation Type", y = "Number of Contributions") + #label the axes
       theme_minimal() + # assign theme
       coord_flip() # flip coordinates
-    plot_donations_by_occupation_2015
 
 # Plot percentage of donations by occupation type in 2015:
     # Reorder data (using Cycle because it's the same value for each observation)
     seattle_occ_2015$OccupationType <- reorder(seattle_occ_2015$OccupationType, as.numeric(seattle_occ_2015$Cycle), sum)
 
+    # Using dataset "seattle_occ_2015", set x to OccupationType, fill by Voucher level, and label the levels.
     ggplot(seattle_occ_2015, aes(OccupationType, fill = factor(Voucher, levels = c(1, 0), labels = c("Voucher", "Non-Voucher")))) + 
       geom_bar(aes(y = (..count..)/sum(..count..)), position = "stack") + # change from count to proportion
       scale_y_continuous(labels = scales::percent) + # convert from proportion to percent
@@ -198,7 +208,8 @@ seattle_occ_2017$Voucher <- as.factor(seattle_occ_2017$Voucher)
     # Reorder Occupation Type by total amount given
     seattle_occ_2017$OccupationType <- reorder(seattle_occ_2017$OccupationType, seattle_occ_2017$Amount, sum)
     
-    plot_amount_by_occupation_2017 <- ggplot(seattle_occ_2017, aes(x = OccupationType, y = Amount, fill = factor(Voucher, levels = c(1, 0), labels = c("Voucher", "Non-Voucher")))) +  
+    # Using dataset "seattle_occ_2018", set x to OccupationType and y to Amount, fill by Voucher level, and label the levels.
+    ggplot(seattle_occ_2017, aes(x = OccupationType, y = Amount, fill = factor(Voucher, levels = c(1, 0), labels = c("Voucher", "Non-Voucher")))) +  
       geom_bar(stat = "identity", position = "stack") +
       scale_fill_discrete(name = "Contribution Type") + # label legend
       labs(title = "2017 Voucher vs. Non-Voucher Dollars Contributed by Occupation Type", # label axes
@@ -206,12 +217,12 @@ seattle_occ_2017$Voucher <- as.factor(seattle_occ_2017$Voucher)
       scale_y_continuous(labels = scales::comma, limits = c(0, 200000)) + # create common scale so we can compare to 2015 plot
       theme_minimal() + # assign theme
       coord_flip() # flip coordinates
-    plot_amount_by_occupation_2017
 
 # Plot 2017 number of donations by occupation type:
     # Reorder Occupation Type by number of donations given (using Cycle because it's the same number for each observation):
     seattle_occ_2017$OccupationType <- reorder(seattle_occ_2017$OccupationType, seattle_occ_2017$Cycle, sum)
 
+    # Using dataset "seattle_occ_2017", set x to OccupationType, fill by Voucher level, and label the levels.
     plot_donations_by_occupation_2017 <- ggplot(seattle_occ_2017, aes(OccupationType, fill = factor(Voucher, levels = c(1, 0), labels = c("Voucher", "Non-Voucher")))) + 
       geom_bar() +
       scale_fill_discrete(name = "Contribution Type") + # label legend
@@ -225,6 +236,7 @@ seattle_occ_2017$Voucher <- as.factor(seattle_occ_2017$Voucher)
     # Reorder data (using Cycle because it's the same value for each observation)
     seattle_occ_2017$OccupationType <- reorder(seattle_occ_2017$OccupationType, as.numeric(seattle_occ_2017$Cycle), sum)
 
+    # Using dataset "seattle_occ_2017", set x to OccupationType, fill by Voucher level, and label the levels.
     ggplot(seattle_occ_2017, aes(OccupationType, fill = factor(Voucher, levels = c(1, 0), labels = c("Voucher", "Non-Voucher")))) + 
       geom_bar(aes(y = (..count..)/sum(..count..)), position = "stack") + # change from count to proportion
       scale_y_continuous(labels = scales::percent) + # convert from proportion to percent
@@ -242,7 +254,8 @@ seattle_occ_2017_cash <- subset(seattle_occ_2017, Voucher==0)
 
     # Reorder OccupationType by total dollars donated
     seattle_occ_2017_vouch$OccupationType <- reorder(seattle_occ_2017_vouch$OccupationType, seattle_occ_2017_vouch$Amount, sum) 
-  
+
+    # Using dataset "seattle_occ_2017_vouch", set x to OccupationType and y to Amount, fill by Voucher level, and label the levels.
     ggplot(seattle_occ_2017_vouch, aes(OccupationType, Amount, fill = factor(Voucher, levels = c(1), labels = c("Voucher")))) +
       geom_bar(stat = "identity") +
       scale_fill_discrete(name = "Contribution Type") + # label legend
@@ -256,6 +269,7 @@ seattle_occ_2017_cash <- subset(seattle_occ_2017, Voucher==0)
     # Reorder OccupationType by total dollars donated
     seattle_occ_2017_cash$OccupationType <- reorder(seattle_occ_2017_cash$OccupationType, seattle_occ_2017_cash$Amount, sum)
     
+    # Using dataset "seattle_occ_2017_cash", set x to OccupationType and y to Amount, fill by Voucher level, and label the levels.
     ggplot(seattle_occ_2017_cash, aes(OccupationType, Amount, fill = factor(Voucher, levels = c(0), labels = c("Non-Voucher")))) +
       geom_bar(stat = "identity") +
       scale_fill_discrete(name = "Contribution Type") + # label legend
@@ -271,6 +285,7 @@ seattle_occ_2017_cash <- subset(seattle_occ_2017, Voucher==0)
     # Reorder Occupation Type by total number of contributions
     seattle_occ_2017_vouch$OccupationType <- reorder(seattle_occ_2017_vouch$OccupationType, seattle_occ_2017_vouch$val, sum)
 
+    # Using dataset "seattle_occ_2017_vouch", set x to OccupationType, fill by Voucher level, and label the levels.
     ggplot(seattle_occ_2017_vouch, aes(x = OccupationType, fill = factor(Voucher, levels = c(1), labels = c("Voucher")))) +
       geom_histogram(stat = "count") +
       scale_fill_discrete(name = "Contribution Type") + # label legend
@@ -285,7 +300,8 @@ seattle_occ_2017_cash <- subset(seattle_occ_2017, Voucher==0)
     
     # Reorder Occupation Type by total number of contributions
     seattle_occ_2017_cash$OccupationType <- reorder(seattle_occ_2017_cash$OccupationType, seattle_occ_2017_cash$val, sum)
-    
+ 
+    # Using dataset "seattle_occ_2017_cash", set x to OccupationType, fill by Voucher level, and label the levels.
     ggplot(seattle_occ_2017_cash, aes(x = OccupationType, fill = factor(Voucher, levels = c(0), labels = c("Non-Voucher")))) +
       geom_histogram(stat = "count") +
       scale_fill_discrete(name = "Contribution Type") + # label legend
@@ -302,30 +318,35 @@ seattle_occ$Cycle <- as.factor(seattle_occ$Cycle)
 seattle_occ$Voucher <- as.factor(seattle_occ$Voucher)
 
 # Plot contributor occupations by election cycle
+
+    # Using dataset "seattle_occ", set x to OccupationType and fill by Cycle
     ggplot(data = seattle_occ, aes(x = OccupationType, fill = Cycle)) + 
-      geom_bar(position = "dodge") +
-      scale_fill_discrete(name = "Cycle") +
+      geom_bar(position = "dodge") + # stack bars next to each other
+      scale_fill_discrete(name = "Cycle") + # label legend
       labs(title = "Contributor Occupations By Election Cycle", subtitle = "2015 vs. 2017",
-           x = "Occupation", y = "Number of Contributions") +
-      scale_x_discrete(labels = function(x) stringr::str_wrap(x, width = 8)) +
-      theme_minimal()
+           x = "Occupation", y = "Number of Contributions") + # label axes and title
+      scale_x_discrete(labels = function(x) stringr::str_wrap(x, width = 8)) + # impose character breaks for the x axis
+      theme_minimal() # apply theme
 
 # Plot number of voucher vs. non-voucher contributions by occupation type and cycle
-    ggplot(data = seattle_occ, aes(x = Cycle, 
-                                   fill = factor(Voucher, levels = c(1, 0), labels = c("Voucher", "Non-Voucher")))) + 
-      geom_bar(stat = "count", position = "stack") +
-      facet_wrap( ~ OccupationType) +
-      scale_fill_discrete(name = "Contribution Type") +
+
+    # Using dataset "seattle_occ," set x to Cycle, fill by Voucher level, and label Voucher levels
+    ggplot(data = seattle_occ, aes(x = Cycle, fill = factor(Voucher, levels = c(1, 0), labels = c("Voucher", "Non-Voucher")))) + 
+      geom_bar(stat = "count", position = "stack") + # stack bars on top of each other
+      facet_wrap( ~ OccupationType) + # facet plot
+      scale_fill_discrete(name = "Contribution Type") + #label legend
       labs(title = "Number of Voucher vs. Non-Voucher Contributions by Occupation Type", subtitle = "2015 vs. 2017",
-           x = "Election Cycle", y = "Number of Contributions") +
-      theme_minimal()
+           x = "Election Cycle", y = "Number of Contributions") + # label axes and title
+      theme_minimal() # apply theme
 
 # Plot total voucher vs. non-voucher dollars contributed by occupation type
+
+    # using dataset "seattle_occ," set x to Cycle and y to Amount, fill by Voucher level, and label Voucher levels
     ggplot(data = seattle_occ, aes(x = Cycle, y = Amount, 
                                    fill = factor(Voucher, levels = c(1, 0), labels = c("Voucher", "Non-Voucher")))) + 
-      geom_bar(stat = "identity", position = "stack") +
-      facet_wrap( ~ OccupationType) +
-      scale_fill_discrete(name = "Contribution Type") +
+      geom_bar(stat = "identity", position = "stack") + # stack bars on top of each other
+      facet_wrap( ~ OccupationType) + # facet plot
+      scale_fill_discrete(name = "Contribution Type") + # label legend
       labs(title = "Total Voucher vs. Non-Voucher Dollars Contributed by Occupation Type", subtitle = "2015 vs. 2017",
-           x = "Election Cycle", y = "Total Dollars Contributed") +
-      theme_minimal()
+           x = "Election Cycle", y = "Total Dollars Contributed") + # label axes and title
+      theme_minimal() # apply theme
